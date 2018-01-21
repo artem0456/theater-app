d9bc09da3d37805bb3f5cc6ab2c8c8ada64f7917
Artem Smirnov <Artem_Smirnov@epam.com>
2018-01-22 00:33:25 +0300

    implement AuditoriumServise interface

--- src/main/java/ru/epam/spring/hometask/service/Impl/AuditoriumServiceImpl.java
+++ src/main/java/ru/epam/spring/hometask/service/Impl/AuditoriumServiceImpl.java
@@ -5,18 +5,33 @@ import ru.epam.spring.hometask.service.AuditoriumService;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 public class AuditoriumServiceImpl implements AuditoriumService {
+
+    private Map<String, Auditorium> auditoriums;
+
     @Nonnull
     @Override
     public Set<Auditorium> getAll() {
-        return null;
+     return (Set<Auditorium>) auditoriums.values();
     }
 
     @Nullable
     @Override
     public Auditorium getByName(@Nonnull String name) {
-        return null;
+        return auditoriums.get(name);
+    }
+
+//    public Map<String, Auditorium> getAuditoriums() {
+//        return auditoriums;
+//    }
+
+    public void setAuditoriums(Map<String, Auditorium> auditoriums) {
+        this.auditoriums = auditoriums;
     }
 }
+
